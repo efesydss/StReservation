ab8e2c623261d2b9390d88e7c7ae70f5c77ca39b
efesydss <40852666+efesydss@users.noreply.github.com>
2022-05-26 02:00:21 +0300

    pageOne finished
    only frontend

--- StRandevu/app/src/main/java/com/example/strandevu/MainActivity.java
+++ StRandevu/app/src/main/java/com/example/strandevu/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.strandevu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView btnSignIn,btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignIn=findViewById(R.id.btnSignIn);
+        btnSignUp=findViewById(R.id.btnSignUp);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SignUp Çalıştı", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "SignIn Çalıştı", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
