c2d26aadcdb4813be32390b7eb084b385e70f385
efesydss <40852666+efesydss@users.noreply.github.com>
2022-05-27 18:28:59 +0300

    bottom nav

--- StRandevu/app/src/main/java/com/example/strandevu/HomeActivity.java
+++ StRandevu/app/src/main/java/com/example/strandevu/HomeActivity.java
@@ -1,14 +1,24 @@
 package com.example.strandevu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class HomeActivity extends AppCompatActivity {
+    private BottomNavigationView bottomtut;
+    private Fragment temp;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //getSupportFragmentManager().beginTransaction().add(R.id.fragmenttutucu,new fragmentClass()).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragmentTutucu,new fragmentClass()).commit();
+        
     }
 }
